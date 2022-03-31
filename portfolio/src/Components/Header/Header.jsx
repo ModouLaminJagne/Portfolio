7496c55bd301a030ac399edb8f3dbd9846b90fa8
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-31 23:20:45 +0000

    Header component added.

--- portfolio/src/Components/Header/Header.jsx
+++ portfolio/src/Components/Header/Header.jsx
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import './Header.css';
-// import CTA from './CTA';
-// import mo from '../../Assets/mo.png';
-// import HeaderSocials from './HeaderSocials';
+import React from 'react';
+import './Header.css';
+import CTA from './CTA';
+import mo from '../../Assets/mo.png';
+import HeaderSocials from './HeaderSocials';
 
-// const Header = () => {
-//   return (
-//     <header>
-//       <div id='home' className="container header__container">
-//         <h5>Hello I'm</h5>
-//         <h1>Modou Jallow</h1>
-//         <h5 className="text-white">React Developer</h5>
-//         <CTA />
-//         <HeaderSocials />
-//         <div className="me">
-//           <img src={mo} alt="me" />
-//         </div>
+const Header = () => {
+  return (
+    <header>
+      <div id='home' className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Modou Jallow</h1>
+        <h5 className="text-white">React Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={mo} alt="me" />
+        </div>
 
-//         <a href="#contact" className="scroll__down">
-//           Scroll Down
-//         </a>
-//       </div>
-//     </header>
-//   );
-// };
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
-// export default Header;
+export default Header;
