9c9db2bdeb2555b9f25aea727de887a581794956
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-04-08 18:52:09 +0000

    packages installed.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,27 +1,27 @@
 import './App.css';
 import Header from './Components/Header/Header';
-// import Nav from './Components/Nav/Nav';
-// import About from './Components/About/About';
-// import Services from './Components/Services/Services';
-// import Experience from './Components/Experience/Experience';
-// import Footer from './Components/Footer/Footer';
-// import Contacts from './Components/Contact/Contacts';
-// import Portfolio from './Components/Portfolio/Portfolio';
-// import Testimonials from './Components/Testimonial/Testimonials';
+import Nav from './Components/Nav/Nav';
+import About from './Components/About/About';
+import Services from './Components/Services/Services';
+import Experience from './Components/Experience/Experience';
+import Footer from './Components/Footer/Footer';
+import Contacts from './Components/Contact/Contacts';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Testimonials from './Components/Testimonial/Testimonials';
 import React from 'react';
 
 const App = () => {
   return (
     <div className="bg">
       <Header />
-      {/* <Nav />
+      <Nav />
       <About />
       <Experience />
       <Services />
       <Portfolio />
       <Testimonials />
       <Contacts />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 };
